ba65875603bd5ba02ab75ec725ef25acb376b2a9
haru-oss <haruyaishungry@gmail.com>
2024-01-15 19:50:12 +0900

    モデルを学習し、モデルの定義まで終えました

--- form_6/blog/models.py
+++ form_6/blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Friends(models.Model):
+
+    name=models.CharField(max_length=255)
+
+
+
+
+
+    mail=models.EmailField()
+    
+    
+    
+    
+    gender=models.BooleanField()
+    
+    
+    
+    age=models.IntegerField(defalt=0)
+    
+    
+    
+    birthday=models.DateField()
+    
+
+
+    def __str__(self):
+        return '<Friend:id='  +  str(self.id)  +  ','  + \
+            self.name  +  '('  +  str(self.age)  +  ')>'
+
+
+
